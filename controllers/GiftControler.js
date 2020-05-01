5385308bdb6c26754f3f5ce6774e9c673b04c8d7
Antonin <antonin.demaneche@ynov.com>
2020-05-01 14:10:28 +0200

    Commit : Ajout / Delete Non fonctionel

--- controllers/GiftControler.js
+++ controllers/GiftControler.js
@@ -1,5 +1,11 @@
-var data = [{item: 'giff1'}, {item:'gif2'}, {item: 'giff3'}];
 
+
+
+var bodyParser = require('body-parser');
+
+
+var data = [{item: 'giff1'}, {item:'gif2'}, {item: 'giff3'}];
+var urlencodedParser = bodyParser.urlencoded({extended: false});
 
 module.exports = function(app){
 
@@ -7,12 +13,16 @@ app.get('/gift', function(req, res){
     res.render('gift', {gifts: data});
 });
 
-app.post('/gift', function(req, res){
-
+app.post('/gift', urlencodedParser, function(req, res){
+    data.push(req.body);
+    res.json(data);
 });
 
-app.delete('/gift', function(req, res){
-
+app.delete('/gift/:item', function(req, res){
+    data = data.filter(function(gift){
+        return gift.item.replace(/ /g, '-') !== req.params.item;
+    });
+    res.json(data);
 });
 
 };
