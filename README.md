71fbdb169ed4b7afde53c9ed6dbd3a6f92115fc6
Antonin <antonin.demaneche@ynov.com>
2020-05-01 13:30:48 +0200

    Commit : Probléme de Style

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
-orm_api_ap
+# Commande Executer 
+
+```
+npm init
+```
+
+```
+npm install express -save
+```
+
+```
+npm install ejs -save
+```
+
+```
+npm install body-parser -save
+```
+```
+npm install -g nodemon
+```
---
+++ controllers/GiftControler.js
@@ -0,0 +1,15 @@
+module.exports = function(app){
+
+app.get('/gift', function(req, res){
+    res.render('gift');
+});
+
+app.post('/gift', function(req, res){
+
+});
+
+app.delete('/gift', function(req, res){
+
+});
+
+};
