bb22ddd9080741b0315b4b1e4a618916a93e4162
Antonin <antonin.demaneche@ynov.com>
2020-05-01 13:43:45 +0200

    Commit : Probléme de Style résolut

--- controllers/GiftControler.js
+++ controllers/GiftControler.js
@@ -1,7 +1,10 @@
+var data = [{item: 'giff1'}, {item:'gif2'}, {item: 'giff3'}];
+
+
 module.exports = function(app){
 
 app.get('/gift', function(req, res){
-    res.render('gift');
+    res.render('gift', {gifts: data});
 });
 
 app.post('/gift', function(req, res){
