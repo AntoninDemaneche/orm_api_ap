835b4d8576c217345538910c0c76aa57cf9540c2
Antonin <antonin.demaneche@ynov.com>
2020-05-02 15:15:48 +0200

    Autre modification sur la bdd

--- dbcreate.js
+++ dbcreate.js
@@ -7,6 +7,7 @@ let db = new sqlite3.Database(db_name, err => {
         throw err
     console.log('La bdd se lance sur '+db_name)
 
-    db.run(`CREATE TABLE playlist(id INTERGER PRIMARYKEY,name VARCHAR(255) )`)
-    db.run(`INSERT INTO playlist(name) VALUES("I am blue")`)
+    //db.run(`CREATE TABLE playlist(id INTERGER PRIMARYKEY,name VARCHAR(255) )`)
+    //db.run(`INSERT INTO playlist(name) VALUES("Test3")`)
+    console.log('Les commandes ce sont executer')
 })
