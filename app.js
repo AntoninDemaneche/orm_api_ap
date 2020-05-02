0315bf7e14023d28164082bebaf8581a4d43304c
PierreBouffartigue <43409971+PierreBouffartigue@users.noreply.github.com>
2020-05-02 14:27:30 +0200

    Fix errors

--- app.js
+++ app.js
@@ -12,29 +12,32 @@ app.set('view engine', 'ejs');
 
 app.use('/assets', express.static('./public'))
 
-//const sequelize = new Sequelize('sqlite;chinook.db');
-//
-//const Playlist = sequelize.define('playlist', {
-//    id: {
-//        field: 'PlaylistId',
-//        type: Sequelize.INTEGER,
-//        primaryKey: true
-//    },
-//    name:{
-//        field: 'Name',
-//        type : Sequelize.STRING
-//    }
-//}, {
-//    timestamps: false
-//});
-//
-//app.get('/api/playlist', function(request, reponse){
-//    Playlist.findAll().then((playlist) => {
-//        response.json(playlist);
-//    });
-//});
-//
-//
+const sequelize = new Sequelize('database', 'username', 'password', {
+    host: 'localhost',
+    dialect: 'sqlite'
+  });
+
+const Playlist = sequelize.define('playlist', {
+    id: {
+        field: 'PlaylistId',
+        type: Sequelize.INTEGER,
+        primaryKey: true
+    },
+    name:{
+        field: 'Name',
+        type : Sequelize.STRING
+    }
+}, {
+    timestamps: false
+});
+
+app.get('/api/playlist', function(request, reponse){
+   Playlist.findAll().then((playlist) => {
+       response.json(playlist);
+    });
+});
+
+
 giftController(app);
 
 
